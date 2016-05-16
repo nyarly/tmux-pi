05bb351353c6c508e56a3cfce8bf4cde221d8b27
Judson <nyarly@gmail.com>
2016-05-16 08:08:48 -0700

    Lifetime and references stage

--- src/lib.rs
+++ src/lib.rs
@@ -5,55 +5,74 @@ use std::sync::mpsc::{Receiver, Sender, channel};
 extern crate regex;
 
 struct TmuxControl {
-  tmux: Child,
+  //tmux: Child,
   reader: JoinHandle<()>,
   writer: JoinHandle<()>,
   commands: Sender<Pair>,
 }
 
-struct Pair(Box<TmuxCommand<R=Response>>, Sender<Box<Response>>);
+struct Pair(Box<TmuxCommand>, Sender<Box<Response>>);
 struct SeqRs(u64, Box<Response>, Sender<Box<Response>>);
 
 impl TmuxControl {
   fn start() -> TmuxControl {
-    let cmd = Command::new("tmux")
+    let child = Command::new("tmux")
                 .arg("-C")
                 .env_remove("TMUX")
                 .stdin(Stdio::piped())
-                .stdout(Stdio::piped());
-    let child = cmd.spawn().unwrap();
+                .stdout(Stdio::piped())
+                .spawn()
+                .unwrap();
     let (responses, send) = channel();
     let (tell, commands) = channel();
-    let writer = thread::spawn(move || write_command_stream(commands, responses, child.stdin.expect("didn't open a stdin pipe?!")));
-    let reader = thread::spawn(move || read_command_stream(child.stdout.expect("didn't open a stdout pipe?!"), send));
+    let stdin = child.stdin.expect("didn't open a stdin pipe?!");
+    let stdout = child.stdout.expect("didn't open a stdout pipe?!");
+    let writer = thread::spawn(move || write_command_stream(commands, responses, stdin));
+    let reader = thread::spawn(move || read_command_stream(stdout, send));
 
     TmuxControl {
-      tmux: child,
+      // tmux: child, //how do we kill the child tmux process?
       reader: reader,
       writer: writer,
       commands: tell,
     }
   }
 
-  fn transact(&self, cmd: Box<TmuxCommand<R=Response>>) -> Receiver<Box<Response>> {
+  fn transact(&self, cmd: Box<TmuxCommand>) -> Receiver<Box<Response>> {
     let (tx, rx) = cmd.response_channel();
     self.commands.send(Pair(cmd, tx));
     rx
   }
 }
 
-trait TmuxCommand : Send{
-  type R: Response;
+fn write_command_stream(commands: Receiver<Pair>, results: Sender<SeqRs>, mut stdin: ChildStdin) {
+  let message: String;
+  let mut seq = 0;
+  loop {
+    let Pair(command, tx) = commands.recv().unwrap();
+    seq += 1;
+    results.send(SeqRs(seq, command.build_response(), tx));
+    let res = stdin.write(command.wire_format());
+    res.unwrap();
+  }
+}
 
-  fn response_channel(&mut self) -> (Sender<Box<Self::R>>, Receiver<Box<Self::R>>) {
+fn read_command_stream(stdout: ChildStdout, send_channels: Receiver<SeqRs>) {
+  let mut csr = CommandStreamReader::new();
+  csr.read(stdout, send_channels)
+}
+
+
+trait TmuxCommand : Send{
+  fn response_channel(&self) -> (Sender<Box<Response>>, Receiver<Box<Response>>) {
     channel()
   }
-  fn build_response(&self) -> Box<Self::R>;
+  fn build_response(&self) -> Box<Response>;
   fn wire_format(&self) -> &'static [u8];
 }
 
 trait Response : Send {
-  fn consume(&mut self, text: &[u8]);
+  fn consume(&mut self, text: &String);
 }
 
 struct InfoCmd;
@@ -63,9 +82,7 @@ struct InfoResponse {
 }
 
 impl TmuxCommand for InfoCmd {
-  type R = InfoResponse;
-
-  fn build_response(&self) -> Box<InfoResponse> {
+  fn build_response(&self) -> Box<Response> {
     Box::new(InfoResponse::new())
   }
   fn wire_format(&self) -> &'static [u8] {
@@ -74,8 +91,8 @@ impl TmuxCommand for InfoCmd {
 }
 
 impl Response for InfoResponse {
-  fn consume(&mut self, text: &[u8]) {
-    self.content = String::from_utf8_lossy(text).into_owned()
+  fn consume(&mut self, text: &String) {
+    self.content = text.clone()
   }
 }
 
@@ -95,56 +112,125 @@ struct RawResponse {
 use regex::{Captures, Regex};
 use std::io::Write;
 
-fn write_command_stream(commands: Receiver<Pair>, results: Sender<SeqRs>, stdin: ChildStdin) {
-  let message: String;
-  let seq = 0;
-  loop {
-    let Pair(command, tx) = commands.recv().unwrap();
-    seq += 1;
-    results.send(SeqRs(seq, command.build_response(), tx));
-    let res = stdin.write(command.wire_format());
-    res.unwrap();
-  }
+struct CommandStreamReader {
+  buffer: [u8; 4096],
+  start: usize,
+  end: usize,
+  max: usize,
+  half: usize,
+  maybe_output: Option<String>,
+  response_mode: bool,
+  max_end_length: usize,
+  results: Vec<RawResponse>,
+  stanza_re: Regex,
+  stanza_end_re: Regex,
+  arg_re: Regex,
 }
 
-fn read_command_stream(stdout: ChildStdout, send_channels: Receiver<SeqRs>) {
-  let mut buffer = [0; 4096];
-  let (start, end, max) = (0, 0, 4096);
-  let half = max / 2;
-  let response_mode = false;
-  let maybe_output: Option<String> = None;
-  let mut results = Vec::new();
+type StreamBuffer<'a> = &'a [u8];
 
-  let stanza_re = Regex::new("(?m)^%(begin|exit|layout-change|output|session-changed|\
-    session-renamed|sessions-changed|unlinked-window-add|window-add|window-close|\
-    window-renamed)")
-                    .unwrap();
-  let stanza_end_re = Regex::new("(?m)^%(end|error)").unwrap();
-  let max_end_length = 6;
+use std::{str, u64};
+use std::str::FromStr;
+use std::io::Read;
+use std::sync::mpsc::TryRecvError;
+impl  CommandStreamReader  {
+  fn new() -> CommandStreamReader {
+    let max = 4096;
+    CommandStreamReader{
+      buffer: [0; 4096],
+      start: 0,
+      end: 0,
+      max: max,
+      half : max / 2,
+      response_mode : false,
+      maybe_output: None,
+      results : Vec::new(),
 
-  let arg_re = Regex::new(r"^(\s+(?<first>\S+))?(\s+(?<second>\S+))?(\s+(?<third>\S+))?\s*\n");
+      stanza_re : Regex::new("(?m)^%(begin|exit|layout-change|output|session-changed|\
+      session-renamed|sessions-changed|unlinked-window-add|window-add|window-close|\
+      window-renamed)").unwrap(),
+      stanza_end_re : Regex::new("(?m)^%(end|error)").unwrap(),
+      max_end_length : 6,
 
-  let advance_buffer = |captures: &Captures| {
+      arg_re : Regex::new(r"^(\s+(?<first>\S+))?(\s+(?<second>\S+))?(\s+(?<third>\S+))?\s*\n").unwrap(),
+    }
+  }
+
+  fn read(&mut self, mut stdout: ChildStdout, send_channels: Receiver<SeqRs>) {
+    loop {
+      match stdout.read(&mut self.buffer[self.end..(self.max - 1)]) {
+        Ok(num) => self.end += num,
+        Err(err) => panic!(err),
+      }
+
+      match self.maybe_output {
+        None => self.capture_notification(),
+        Some(ref block) => self.capture_output_block(block),
+      }
+
+      while self.results.len() > 0 {
+        match send_channels.try_recv() {
+          Err(TryRecvError::Empty) => break,
+          Err(_) => return,
+          Ok(SeqRs(num, ref resp, ref tx)) => {
+            let pos = self.results.iter()
+                             .position(|&r| r.num == num)
+                             .expect("received response ahead of command");
+            let res = self.results.swap_remove(pos);
+            resp.consume(&(res.output));
+            tx.send(*resp).unwrap_or(())
+          }
+        }
+      }
+
+      if self.start > self.half {
+        self.buffer[..(self.end - self.start)].clone_from_slice(&self.buffer[self.start..self.end]);
+        self.end = self.end - self.start;
+        self.start = 0
+      }
+    }
+  }
+
+  fn advance_buffer(&mut self, captures: &Captures) {
     let (b, e) = captures.pos(0).unwrap();
-    start += e;
-  };
+    self.start += e;
+  }
 
-  let get_args = || -> (Option(&str), Option(&str), Option(&str)) {
-    let captures = arg_re.captures(buffer[start..end]).unwrap();
-    advance_buffer(captures);
-    (captures.at(1), captures.at(2), captures.at(3));
-  };
+  fn buffer_prefix(&self, end: usize) -> StreamBuffer {
+    &self.buffer[self.start..end]
+  }
 
-  let capture_notification = || {
-    match stanza_re.captures(buffer[start..end]) {
+  fn valid_str(&self, end: usize) -> &str {
+    match str::from_utf8(self.buffer_prefix(end)) {
+      Ok(s) => s,
+      Err(e) => str::from_utf8(&self.buffer_prefix(end)[..e.valid_up_to()]).unwrap()
+    }
+  }
+
+  fn current_buffer(&self) -> StreamBuffer {
+    self.buffer_prefix(self.end)
+  }
+
+  fn current_str(&self) -> &str {
+    self.valid_str(self.end)
+  }
+
+  fn get_args(&mut self) -> (Option<&str>, Option<&str>, Option<&str>) {
+    let captures = self.arg_re.captures(self.current_str()).unwrap();
+    self.advance_buffer(&captures);
+    (captures.at(1), captures.at(2), captures.at(3))
+  }
+
+  fn capture_notification(&mut self) {
+    match self.stanza_re.captures(self.current_str()) {
       None => (),
       Some(captures) => {
-        advance_buffer(&captures);
+        self.advance_buffer(&captures);
 
         match captures.at(1).unwrap() {
           "begin" => {
-            get_args();
-            maybe_output = Some(String::from(""))
+            self.get_args();
+            self.maybe_output = Some(String::from(""))
           }
           "exit" => (),
           "layout-change" => (),
@@ -156,66 +242,36 @@ fn read_command_stream(stdout: ChildStdout, send_channels: Receiver<SeqRs>) {
           "window-add" => (),
           "window-close" => (),
           "window-renamed" => (),
+          _ => (), //maybe a warning or something...
         }
       }
     }
-  };
+  }
 
-  let capture_output_block = |block| {
-    let limit: u16;
-    match stanza_end_re.captures(buffer[start..end]) {
+  fn capture_output_block(&mut self, block: &String) {
+    let limit: usize;
+    match self.stanza_end_re.captures(self.current_str()) {
       None => {
-        limit = end - (max_end_length);
-        block.extend(buffer[start..limit]);
-        start = limit;
+        limit = self.end - (self.max_end_length);
+        block.push_str(self.current_str());
+        self.start = limit;
       }
       Some(capture) => {
-        let (limit, finish) = capture.pos(0);
-        let (timestamp, number, flags) = get_args();
-        block.extend(buffer[start..limit]);
-        start = limit;
+        let (limit, finish) = capture.pos(0).expect("the zero capture should always exist, right?");
+        let (timestamp, number, flags) = self.get_args();
+        block.push_str(self.valid_str(limit));
+        self.start = limit;
         if flags.is_none() {
           return;
         }
-        start = finish;
-        maybe_output = None;
-        results.push(RawResponse {
-          num: u64::from_str(number).unwrap(),
-          output: block,
+        self.start = finish;
+        self.maybe_output = None;
+        self.results.push(RawResponse {
+          num: u64::from_str(number.expect("if flags is present, so much number be")).unwrap(),
+          output: block.clone(),
         })
       }
     }
-  };
-
-  loop {
-    match stdout.read(&mut buffer[end..(max - 1)]) {
-      Ok(num) => end += num,
-      Err(err) => panic!(err),
-    }
-
-    match maybe_output {
-      None => capture_notification(),
-      Some(block) => capture_output_block(&block),
-    }
-
-    while results.len() > 0 {
-      match send_channels.try_recv() {
-        Err(Empty) => break,
-        Err(_) => return,
-        Ok(SeqRs(num, resp, tx)) => {
-          let pos = results.iter()
-                           .position(|&r| r.num == num)
-                           .expect("received response ahead of command");
-          let res = results.swap_remove(pos);
-          resp.consume(res.output);
-          tx.send(resp)
-        }
-      }
-    }
-
-    if start > half {
-      buffer[..(end - start)].clone_from_slice(buffer[start..end]);
-      (start, end) = (0, end - start)
-    }
   }
+
 }
